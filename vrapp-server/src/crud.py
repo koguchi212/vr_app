493c962202216678a7e2d2858d2fbf702d284fc8
koguchi212 <akira.kouguchi@gmail.com>
2023-06-30 19:25:34 +0900

    delete関数をcrudに追加しました

--- vrapp-server/src/crud.py
+++ vrapp-server/src/crud.py
@@ -11,11 +11,20 @@ def add_glb_file(prompt, file_path):
     c.execute("INSERT INTO glb_files (prompt, file_path) VALUES (?, ?)", (prompt, file_path))
     conn.commit()
 
-# 使用例
-prompt = "example prompt"
-file_path = "example_file.glb"
-add_glb_file(prompt, file_path)
+# promptに一致するレコードを削除する関数
+def delete_glb_file_by_prompt(prompt):
+    c.execute("DELETE FROM glb_files WHERE prompt=?", (prompt,))
+    conn.commit()
 
+# # 使用例
+# prompt = "example prompt"
+# file_path = "example_file.glb"
+
+# # レコードの追加
+# add_glb_file(prompt, file_path)
+
+# # レコードの削除
+# delete_glb_file_by_prompt(prompt)
 
 # データベース接続を閉じる
 conn.close()
